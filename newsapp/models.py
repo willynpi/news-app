e50c58ca0e6d17f7ada001d2f6384e010efb514d
Ying <afji2039@gmail.com>
2018-10-27 13:00:07 +0800

    create NewsUnit

--- newsapp/models.py
+++ newsapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class NewsUnit(models.Model):
+    category = models.CharField(max_length=10, null=False)
+    author = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, null=False)
+    message = models.TextField(null=False)
+    pubtime = models.DateTimeField(auto_now=True) #自動取得目前時間
+    enabled = models.BooleanField(default=False)
+    press = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.title
+
